21af0498dde81781c968a031247100087c1b8aa3
Everton Santos <evertonricardo036@gmail.com>
2026-04-30 21:51:42 -0300

    feat: add POST /clinics route

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const clinics = [
     {
         id: 1, 
-        name: 'Clínica Exemplo',
+        name: 'Clinica Exemplo',
         cnpj: '00.000.000/0001-00'
     }
 ];
@@ -19,6 +19,20 @@ app.get('/', (req, res) => {
 
 app.get('/clinics', (req, res) => {
     res.json(clinics);
+});
+
+app.post('/clinics', (req, res) => {
+    const { name, cnpj } = req.body
+
+    const newClinic = {
+        id: clinics.length + 1,
+        name,
+        cnpj
+    };
+
+    clinics.push(newClinic);
+
+    res.status(201).json(newClinic);
 });
 
 app.listen(PORT, () => {
